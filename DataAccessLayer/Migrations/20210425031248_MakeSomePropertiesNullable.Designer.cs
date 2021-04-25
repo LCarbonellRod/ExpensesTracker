ca865927b314cc7cbc6f3a64f2bf8d524e1ed954
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2021-04-25 02:00:51 -0300

    Add Database changes

--- DataAccessLayer/Migrations/20210425031248_MakeSomePropertiesNullable.Designer.cs
+++ DataAccessLayer/Migrations/20210425031248_MakeSomePropertiesNullable.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(ExpensesDbContext))]
-    partial class ExpensesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210425031248_MakeSomePropertiesNullable")]
+    partial class MakeSomePropertiesNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -69,9 +71,6 @@ namespace DataAccessLayer.Migrations
                         .HasColumnType("bit");
 
                     b.Property<DateTime>("FechaCreacion")
-                        .HasColumnType("datetime2");
-
-                    b.Property<DateTime>("FechaInicioPago")
                         .HasColumnType("datetime2");
 
                     b.Property<DateTime?>("FechaPagado")
--- DataAccessLayer/Migrations/20210425032128_AddFechaInicioProperty.Designer.cs
+++ DataAccessLayer/Migrations/20210425032128_AddFechaInicioProperty.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(ExpensesDbContext))]
-    partial class ExpensesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210425032128_AddFechaInicioProperty")]
+    partial class AddFechaInicioProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
