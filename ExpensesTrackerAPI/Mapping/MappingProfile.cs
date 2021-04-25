e12864ab5032802f3ac55cc41171b19a61e73e37
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2021-04-25 02:02:58 -0300

    Add automapper and resources

---
+++ ExpensesTrackerAPI/Mapping/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using ExpensesTrackerAPI.Resources;
+using ExpensesTrackerCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesTrackerAPI.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Resource
+            CreateMap<Gasto, GastoResource>();
+            CreateMap<Cuota, CuotaResource>();
+
+            // Resource to Domain
+            CreateMap<GastoResource, Gasto>();
+            CreateMap<CuotaResource, Cuota>();
+        }
+    }
+}
---
+++ ExpensesTrackerAPI/Resources/CuotaResource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpensesTrackerAPI.Resources
+{
+    public class CuotaResource
+    {
+        public Guid Id { get; set; }
+        public int CuotaNumero { get; set; }
+        public bool CuotaPagada { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public decimal ValorCuota { get; set; }
+        public DateTime? FechaPagado { get; set; }
+
+        public GastoResource Gasto { get; set; }
+    }
+}
