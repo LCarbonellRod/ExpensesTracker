ed8b9ac43b2e63cdddfc70050adfb4c617bf0aec
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2021-04-25 22:57:01 -0300

    Add new methods to Gastos controller

--- ExpensesTrackerAPI/Controllers/GastosController.cs
+++ ExpensesTrackerAPI/Controllers/GastosController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using ExpensesTracker.Services.Services;
 using ExpensesTrackerAPI.Resources;
+using ExpensesTrackerAPI.Validators;
 using ExpensesTrackerCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -40,6 +41,78 @@ namespace ExpensesTrackerAPI.Controllers
             var gastoResources = _mapper.Map<Gasto, GastoResource>(gasto);
 
             return Ok(gastoResources);
+        }
+
+        [HttpPost("")]
+        public async Task<ActionResult<GastoResource>> CreateGasto([FromBody] SaveGastoResource saveGastoResource)
+        {
+            var validator = new SaveGastoResourceValidator();
+            var validationResult = await validator.ValidateAsync(saveGastoResource);
+
+            if (!validationResult.IsValid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
+            var gastoToCreate = _mapper.Map<SaveGastoResource, Gasto>(saveGastoResource);
+
+            var newGasto = await _gastoService.CreateGasto(gastoToCreate);
+
+            var gasto = await _gastoService.GetGastoById(newGasto.Id);
+
+            var gastoResource = _mapper.Map<Gasto, GastoResource>(gasto);
+
+            return Ok(gastoResource);
+            
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<GastoResource>> UpdateGasto(Guid id, [FromBody] SaveGastoResource saveGastoResource)
+        {
+            var validator = new SaveGastoResourceValidator();
+            var validationResult = await validator.ValidateAsync(saveGastoResource);
+
+
+            var requestIsInvalid = id == null || !validationResult.IsValid;
+
+            if (requestIsInvalid)
+            {
+                return BadRequest(validationResult.Errors);
+            }
+
+            var gastoToBeUpdated = await _gastoService.GetGastoById(id);
+
+            if (gastoToBeUpdated == null)
+            {
+                return NotFound();
+            }
+
+            var gasto = _mapper.Map<SaveGastoResource, Gasto>(saveGastoResource);
+
+            await _gastoService.UpdateGasto(gastoToBeUpdated, gasto);
+
+            var updatedGasto = await _gastoService.GetGastoById(id);
+
+            var updateGastoResource = _mapper.Map<Gasto, GastoResource>(updatedGasto);
+
+            return Ok(updateGastoResource);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGasto(Guid id)
+        {
+            if (id == null)
+                return BadRequest();
+
+            var gasto = await _gastoService.GetGastoById(id);
+
+            if (gasto == null)
+                return NotFound();
+
+            await _gastoService.DeleteGasto(gasto);
+
+            return NoContent();
         }
     }
 }
